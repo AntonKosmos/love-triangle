233309edeb3395c35d90db6fb5f55c382a9ee5ce
Антон Н. Космачев <kan@vez.local>
2019-03-14 14:07:05 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,21 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+var arr = [];
+ var count = 0;
+ var first_index, second_index;
+ preferences.unshift(0);
+  for(var i = 1; i < preferences.length; i ++) {
+  	if(arr.indexOf(i) == -1)
+  	{
+
+  		first_index = preferences[i]; 
+  		second_index = preferences[preferences[first_index]]; 
+  		if(i == second_index && preferences[first_index] != first_index) {
+  		  count ++;
+        arr.push(first_index, preferences[first_index]);
+        }
+      }
+   }
+return count;
 };
